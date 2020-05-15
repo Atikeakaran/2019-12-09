55150996ab1e6366751fe527ee0dba6f8723f9ee
Atikeakaran <57366269+Atikeakaran@users.noreply.github.com>
2020-05-16 00:22:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//this is first console log in js file
+
+console.log('Atike');
+console.log('my 4. log');
+alert("Hello World");
+
+var myFirstVar; 
+//bellek üzerinde bir alan oluşturur. (değişken tanımlama)
+myFirstVar = "hello Variable world2";
+
+alert(myFirstVar); 
+//this line changes next cconsole log data 
+//myFirstVar = 13;
+
+console.log(myFirstVar);
+
+myFirstVar = 13;
+console.log(myFirstVar);
+
+var mySecvar = 7;
+console.log(myFirstVar + mySecvar);
+
+console.log(parseInt(myFirstVar) + (mySecvar));
+
+var total = mySecvar + myFirstVar;
+console.log(total);
+
+var myText1 = "Hello";
+var myText2 = "world5";
+console.log(myText1 + ' ' + myText2);
+
+
